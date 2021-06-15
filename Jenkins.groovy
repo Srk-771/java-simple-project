93ca9cd9258688167279cbcb187bbf6deea166b1
Sharuk <sharuk.livee@gmail.com>
2021-06-15 14:15:40 +0530

    new project added

---
+++ Jenkins.groovy
@@ -0,0 +1,14 @@
+import hudson.plugins.git.*;
+
+def scm = new GitSCM("https://github.com/Srk-771/java-simple-project.git")
+scm.branches = [new BranchSpec("master")];
+
+def flowDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "Jenkins-Build.groovy")
+
+def parent = Jenkins.instance
+def job = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "java-app")
+def description = "helloworld project"
+def poll = '* * * * *'
+job.definition = flowDefinition
+
+parent.reload()
