36684a1c18a41e264ce753a9162fcfe2100e47c8
Sharuk <sharuk.livee@gmail.com>
2021-06-15 14:19:19 +0530

    new project added

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -3,7 +3,7 @@ import hudson.plugins.git.*;
 def scm = new GitSCM("https://github.com/Srk-771/java-simple-project.git")
 scm.branches = [new BranchSpec("master")];
 
-def flowDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "Jenkins-Build.groovy")
+def flowDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "Jenkinsfile")
 
 def parent = Jenkins.instance
 def job = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "java-app")
